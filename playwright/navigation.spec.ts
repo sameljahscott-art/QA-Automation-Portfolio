ac9fc60fb3e9733420fc58f450c24c35124a2c43
sami <sami@samis-MacBook-Pro.local>
2026-01-08 19:14:21 -0500

    Improve Playwright navigation test with assertions

---
+++ playwright/navigation.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+ 
+await page.goto('https://playwright.dev/');
+await page.getByRole('link', { name: 'Docs' }).click();
+const heading = page.getByRole('heading', { name: 'IntroductionDirect link to' })
+await expect (heading).toHaveText ('Introduction')
+});
